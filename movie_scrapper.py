5ef384d6e1fab95cb5c2a74b99d905ada8540daf
Anuj Mishra <anujssmishra@gmail.com>
2022-10-17 04:30:27 +0530

    Handled exception with year

--- movie_scrapper.py
+++ movie_scrapper.py
@@ -10,25 +10,33 @@ def year_func(h3):
         return re.findall(r'\d+', h3.find('span', {'class': 'lister-item-year'}).get_text())[0]
     except IndexError:
         count += 1
+        title = (h3.find('a')['href'])
+        link = 'https://www.imdb.com' + title
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        date = soup.find('a', {'class': 'ipc-metadata-list-item__list-content-item',
+                                 'class': 'ipc-metadata-list-item__list-content-item--link',
+                                 'href': '{title}releaseinfo?ref_=tt_dt_rdat'.format(title=title)}).get_text()
+        year = re.search(r"\d{4}", date).group(0)
+        return year
 
-def ratings_func(h3):
-    global count
-    try:
-        return re.findall(r'\d+', h3.find('span', {'class': 'lister-item-year'}).get_text())[0]
-    except IndexError:
-        count += 1
 
-def place_func(h3):
-    global count
+def ratings_func(div):
+    # global count
     try:
-        return re.findall(r'\d+', h3.find('span', {'class': 'lister-item-year'}).get_text())[0]
-    except IndexError:
-        count += 1
+        if (len(div.find_all("div")) == 4):
+            return 'No ratings'
+        else:
+            return div.find("div").attrs.get('data-value')
+    except AttributeError:
+        return 'No ratings'
+        # count += 1
+
 
 # Downloading imdb bollywood movies from 2000 to 2022
 count = 0
 movies_list = []
-for pages in range(6351, 6370, 50):
+for pages in range(1, 6370, 50):
     url = 'https://www.imdb.com/search/title/?title_type=feature&release_date=2000-01-01,2022-12-31&countries=in&languages=hi&sort=release_date,asc&start={page}&ref_=adv_nxt'.format(
         page=pages)
     response = requests.get(url)
@@ -37,27 +45,30 @@ for pages in range(6351, 6370, 50):
     movies = soup.select("h3.lister-item-header")
     crew = [" ".join(div.find_all("p")[2].get_text().split()) for div in
             soup.select("div.lister-item.mode-advanced div.lister-item-content")]
-    ratings = ['No ratings' if (len(div.find_all("div")) == 3) else div.find("div").attrs.get('data-value')
-               for div in soup.select("div.ratings-bar")]
+    ratings = [ratings_func(div) for div in soup.select("div.lister-item-content")]
     about = ["".join(div.find_all('p', {'class': 'text-muted'})[1].get_text()).split("\n")[1] for div in
              soup.select("div.lister-item-content")]
     place = [h3.find('span', {'class': 'lister-item-index'}).get_text().replace('.', '') for h3 in movies]
     movie_title = [h3.find('a').get_text() for h3 in movies]
-    # year = [re.findall(r'\d+', h3.find('span', {'class': 'lister-item-year'}).get_text())[0] if (
-    # len(h3.find('span', {'class': 'lister-item-year'}).get_text()) > 0) else '2012' for h3 in movies]
     year = [year_func(h3) for h3 in movies]
-    print(len(place))
-    dict_list = [{"place": place[i],
-                  "movie_title": movie_title[i],
-                  "rating": ratings[i],
-                  "year": year[i],
-                  "star_cast": crew[i],
-                  "about": about[i]
-                  } for i in range(50)]
 
-    movies_list.extend(dict_list)
+    # print(len(place))
     print("Done with page ", pages)
     print(count)
+    # print(year)
+    # break
+    try:
+        dict_list = [{"place": place[i],
+                      "movie_title": movie_title[i],
+                      "rating": ratings[i],
+                      "year": year[i],
+                      "star_cast": crew[i],
+                      "about": about[i]
+                      } for i in range(50)]
+    except IndexError:
+        continue
+
+    movies_list.extend(dict_list)
 
 # print(movies_list)
 print(len(movies_list))
